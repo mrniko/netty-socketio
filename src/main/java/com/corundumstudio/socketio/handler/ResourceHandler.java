8572d178a23a581bfa5912bf4e5317a11caaa122
Nikita <abracham.mitchell@gmail.com>
2014-01-10 14:43:56 +0400

    java.lang.NoSuchMethodError: java.net.URLConnection.getContentLengthLong fixed.  Issue #97

--- src/main/java/com/corundumstudio/socketio/handler/ResourceHandler.java
+++ src/main/java/com/corundumstudio/socketio/handler/ResourceHandler.java
@@ -106,7 +106,8 @@ public class ResourceHandler extends ChannelInboundHandlerAdapter {
                 // create ok response
                 HttpResponse res = new DefaultHttpResponse(HTTP_1_1, HttpResponseStatus.OK);
                 // set Content-Length header
-                setContentLength(res, fileUrl.getContentLengthLong());
+                long fileSize = getHeaderFieldLong(fileUrl, "content-length");
+                setContentLength(res, fileSize);
                 // set Content-Type header
                 setContentTypeHeader(res, fileUrl);
                 // set Date, Expires, Cache-Control and Last-Modified headers
@@ -123,6 +124,11 @@ public class ResourceHandler extends ChannelInboundHandlerAdapter {
             }
         }
         ctx.fireChannelRead(msg);
+    }
+
+    public long getHeaderFieldLong(URLConnection fileUrl, String name) {
+        String value = fileUrl.getHeaderField(name);
+        return Long.parseLong(value);
     }
 
     /*
