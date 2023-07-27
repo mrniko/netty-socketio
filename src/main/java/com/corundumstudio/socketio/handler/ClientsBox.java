58784ea862afa83d5e61b0b329ea43755cbb1868
zhanghaoxin-at-826767166263 <COk6iTbDdOqj/yq53u9n9vPTy+3pmZNcm4mKOhImudI=>
2023-07-27 13:33:25 +0000

    add scheduled task for clear disconnected in case there are other memory leak

--- src/main/java/com/corundumstudio/socketio/handler/ClientsBox.java
+++ src/main/java/com/corundumstudio/socketio/handler/ClientsBox.java
@@ -15,18 +15,50 @@
  */
 package com.corundumstudio.socketio.handler;
 
+import com.corundumstudio.socketio.SocketIOServer;
 import io.netty.channel.Channel;
 import io.netty.util.internal.PlatformDependent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import com.corundumstudio.socketio.HandshakeData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientsBox {
+    private static final Logger log = LoggerFactory.getLogger(ClientsBox.class);
 
     private final Map<UUID, ClientHead> uuid2clients = PlatformDependent.newConcurrentHashMap();
     private final Map<Channel, ClientHead> channel2clients = PlatformDependent.newConcurrentHashMap();
+
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    public ClientsBox() {
+        scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            List<UUID> disconnected = new ArrayList<>();
+            for (Map.Entry<UUID, ClientHead> entry : uuid2clients.entrySet()) {
+                if (!entry.getValue().isConnected()) {
+                    disconnected.add(entry.getKey());
+                }
+            }
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+            }
+            for (UUID uuid : disconnected) {
+                ClientHead clientHead = uuid2clients.get(uuid);
+                log.info("Client with sessionId {}-{} was disconnected but still exists in uuid2clients",
+                        clientHead.getSessionId(), clientHead.getEngineIOVersion());
+                uuid2clients.remove(uuid);
+            }
+        }, 5, 5, java.util.concurrent.TimeUnit.SECONDS);
+    }
 
     // TODO use storeFactory
     public HandshakeData getHandshakeData(UUID sessionId) {
