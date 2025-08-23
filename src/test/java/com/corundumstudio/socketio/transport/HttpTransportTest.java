4bd4aeb0542263f7cca2148dba6e23d4cd515999
NeatGuyCoding <15627489+NeatGuyCoding@users.noreply.github.com>
2025-08-23 11:47:56 +0800

    fix license

--- src/test/java/com/corundumstudio/socketio/transport/HttpTransportTest.java
+++ src/test/java/com/corundumstudio/socketio/transport/HttpTransportTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2023 Nikita Koksharov
+ * Copyright (c) 2012-2025 Nikita Koksharov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.corundumstudio.socketio.transport;
 
 import com.corundumstudio.socketio.Configuration;
